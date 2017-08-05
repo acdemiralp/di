a3d168186f83307ae4a27152dc907f21d646c08c
Demiralp <demiralpali@gmail.com>
2017-08-06 00:54:01 +0200

    Demonstrated gl and render_pass in engine test.

--- tests/engine_test.cpp
+++ tests/engine_test.cpp
@@ -14,9 +14,48 @@ extern "C"
 
 TEST_CASE("Engine is tested.", "[engine]") {
   ne::engine engine;
-  auto display_system  = engine.add_system<ne::display> ();
   auto input_system    = engine.add_system<ne::input>   ();
   auto renderer_system = engine.add_system<ne::renderer>();
-  display_system->create_window();
+  auto display_system  = engine.add_system<ne::display> ();
+  display_system ->create_window  ();
+
+  std::unique_ptr<gl::program>      shader_program;
+  std::unique_ptr<gl::vertex_array> vertex_array  ;
+  renderer_system->add_render_pass(ne::render_pass
+  {
+    [&shader_program, &vertex_array] ()
+    {
+      shader_program = std::make_unique<gl::program>     ();
+      vertex_array   = std::make_unique<gl::vertex_array>();
+
+      gl::shader vertex_shader  (GL_VERTEX_SHADER  );
+      gl::shader fragment_shader(GL_FRAGMENT_SHADER);
+      vertex_shader  .set_source("#version 420 \n layout(location = 0) in vec3 position; out vec3 color; void main() { color = position; gl_Position = vec4(position, 1.0); }");
+      fragment_shader.set_source("#version 420 \n in vec3 color; out vec4 frag_color; void main() { frag_color = vec4(color.x + 0.5, 0.5, color.y + 0.5, 1.0); }");
+      assert(vertex_shader.compile() && fragment_shader.compile());
+
+      shader_program->attach_shader(vertex_shader  );
+      shader_program->attach_shader(fragment_shader);
+      assert(shader_program->link());
+
+      float points[] = { -1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, -1.0f, 0.0f, -1.0f, -1.0f, 0.0f, -1.0f, 1.0f, 0.0f };
+      gl::buffer vertex_buffer;
+      vertex_buffer.set_data_immutable(16 * sizeof(float), points);
+
+      vertex_array->set_attribute_enabled(0, true);
+      vertex_array->set_vertex_buffer    (0, vertex_buffer, 0, 3 * sizeof(float));
+    },
+    [&shader_program, &vertex_array] ()
+    {
+      vertex_array  ->bind  ();
+      shader_program->use   ();
+
+      glDrawArrays(GL_TRIANGLE_STRIP, 0, 5);
+
+      shader_program->unuse ();
+      vertex_array  ->unbind();
+    }
+  });
+
   engine.run();
 }
