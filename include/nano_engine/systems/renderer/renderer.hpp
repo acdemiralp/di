19bf7d39686399edb76c048b82a20972967d2ac6
Demiralp <demiralpali@gmail.com>
2017-11-10 02:20:05 +0100

    Remove graphics API related content.

--- include/nano_engine/systems/renderer/renderer.hpp
+++ include/nano_engine/systems/renderer/renderer.hpp
@@ -1,11 +1,9 @@
 #ifndef NANO_ENGINE_SYSTEMS_RENDERER_RENDERER_HPP_
 #define NANO_ENGINE_SYSTEMS_RENDERER_RENDERER_HPP_
 
-#include <gl/all.hpp>
+#include <vector>
 
-#include <nano_engine/engine.hpp>
 #include <nano_engine/system.hpp>
-#include <nano_engine/systems/display/display.hpp>
 #include <nano_engine/systems/renderer/render_pass.hpp>
 
 namespace ne
@@ -13,10 +11,7 @@ namespace ne
 class renderer : public system
 {
 public:
-  renderer           () : default_framebuffer_(0)
-  {
-
-  }
+  renderer           ()                      = default;
   renderer           (const renderer&  that) = default;
   renderer           (      renderer&& temp) = default;
   virtual ~renderer  ()                      = default;
@@ -31,23 +26,16 @@ public:
 protected:
   void initialize() override
   {
-    gl::initialize            ();
-    gl::set_clear_color       ({0.0F, 0.0F, 0.0F, 0.0F});
-    gl::set_depth_test_enabled(true);
     for (auto& render_pass : render_passes_)
       render_pass.setup_();
   }
   void update    () override
   {
-    auto size = engine_->get_system<display>()->get_windows()[0]->get_size();
-    gl::set_viewport({0, 0}, {int(size[0]), int(size[1])});
-    gl::clear       (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     for(auto& render_pass : render_passes_)
       render_pass.execute_();
   }
 
-  gl::framebuffer          default_framebuffer_;
-  std::vector<render_pass> render_passes_      ;
+  std::vector<render_pass> render_passes_;
 };
 }
 
