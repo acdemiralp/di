e129e6f38ce21d971aa161c5f03fdb1d38290489
Demiralp <demiralpali@gmail.com>
2017-11-12 23:42:55 +0100

    Added auxiliary display functions.

--- include/nano_engine/systems/display/display_system.hpp
+++ include/nano_engine/systems/display/display_system.hpp
@@ -19,6 +19,13 @@ namespace ne
 class display_system : public system
 {
 public:
+  enum class message_box_level : unsigned
+  {
+    error       = SDL_MESSAGEBOX_ERROR,
+    warning     = SDL_MESSAGEBOX_WARNING,
+    information = SDL_MESSAGEBOX_INFORMATION
+  };
+
   struct display_mode
   {
     explicit display_mode(const SDL_DisplayMode& native) 
@@ -63,6 +70,26 @@ public:
     display_mode               current_display_mode;
     display_mode               desktop_display_mode;
   };
+  struct message_box_color_scheme
+  {
+    std::array<std::uint8_t, 3> background        = { 200, 200, 200 };
+    std::array<std::uint8_t, 3> text              = {   0,   0,   0 };
+    std::array<std::uint8_t, 3> button_border     = { 175, 175, 175 };
+    std::array<std::uint8_t, 3> button_background = { 100, 100, 100 };
+    std::array<std::uint8_t, 3> button_selected   = { 150, 150, 150 };
+
+    SDL_MessageBoxColorScheme to_native() const
+    {
+      return SDL_MessageBoxColorScheme
+      {{
+        { background       [0], background       [1], background       [2]},
+        { text             [0], text             [1], text             [2]},
+        { button_border    [0], button_border    [1], button_border    [2]},
+        { button_background[0], button_background[1], button_background[2]},
+        { button_selected  [0], button_selected  [1], button_selected  [2]}
+      }};
+    }
+  };
 
   display_system           ()
   {
@@ -79,17 +106,6 @@ public:
   display_system& operator=(      display_system&& temp) = default;
 
   // Factory functionality.
-  enum class message_level : unsigned
-  {
-    error       = SDL_MESSAGEBOX_ERROR,
-    warning     = SDL_MESSAGEBOX_WARNING,
-    information = SDL_MESSAGEBOX_INFORMATION
-  };
-  static void create_message_box(std::string title, std::string message, message_level level = message_level::information)
-  {
-    SDL_ShowSimpleMessageBox(static_cast<Uint32>(level), title.c_str(), message.c_str(), nullptr);
-  }
-
   window*                     create_window        ()
   {
     windows_.emplace_back(std::make_unique<window>());
@@ -148,20 +164,55 @@ public:
       });
     return windows;
   }
-
+  
   // Auxiliary functionality.
-  static std::string               current_video_driver   ()
+  static void                       create_message_box   (
+    const std::string&               title        , 
+    const std::string&               message      , 
+    const message_box_level&         level        = message_box_level::information)
+  {
+    SDL_ShowSimpleMessageBox(static_cast<Uint32>(level), title.c_str(), message.c_str(), nullptr);
+  }
+  static std::string                create_message_box   (
+    const std::string&               title        , 
+    const std::string&               message      , 
+    const std::vector<std::string>&  buttons      , 
+    const message_box_level&         level        = message_box_level::information, 
+    const message_box_color_scheme&  color_scheme = message_box_color_scheme())
+  {
+    std::vector<SDL_MessageBoxButtonData> native_buttons;
+    for(int i = buttons.size() - 1; i >= 0; --i)
+      native_buttons.push_back({0, i, buttons[i].c_str()});
+
+    auto native_color_scheme = color_scheme.to_native();
+
+    const SDL_MessageBoxData message_box_data
+    {
+      static_cast<Uint32>(level)      , 
+      nullptr                         , 
+      title  .c_str()                 , 
+      message.c_str()                 ,
+      static_cast<int>(buttons.size()),
+      native_buttons.data()           ,
+      &native_color_scheme
+    };
+
+    auto index = 0;
+    SDL_ShowMessageBox(&message_box_data, &index);
+    return buttons[index];
+  }
+  static std::string                current_video_driver   ()
   {
     return std::string(SDL_GetCurrentVideoDriver());
   }
-  static std::vector<std::string>  video_drivers          ()
+  static std::vector<std::string>   video_drivers          ()
   {
     std::vector<std::string> drivers;
     for(auto i = 0; i < SDL_GetNumVideoDrivers(); ++i)
       drivers.emplace_back(SDL_GetVideoDriver(i));
     return drivers;
   }
-  static std::vector<display_info> displays               ()
+  static std::vector<display_info>  displays               ()
   {
     std::vector<display_info> displays;
     for(auto i = 0; i < SDL_GetNumVideoDisplays(); ++i)
@@ -200,11 +251,11 @@ public:
     }
     return displays;
   }
-  static void                      set_screensaver_enabled(bool enabled)
+  static void                       set_screensaver_enabled(bool enabled)
   {
     enabled ? SDL_EnableScreenSaver() : SDL_DisableScreenSaver();
   }
-  static bool                      screensaver_enabled    ()
+  static bool                       screensaver_enabled    ()
   {
     return SDL_IsScreenSaverEnabled() != 0;
   }
