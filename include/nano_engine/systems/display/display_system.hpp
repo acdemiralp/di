15d7c34c6c7b4aabfc88550faf1a238ea179b2f2
Demiralp <demiralpali@gmail.com>
2017-11-12 22:31:43 +0100

    Display system progress.

--- include/nano_engine/systems/display/display_system.hpp
+++ include/nano_engine/systems/display/display_system.hpp
@@ -1,8 +1,11 @@
-#ifndef NANO_ENGINE_SYSTEMS_DISPLAY_DISPLAY_HPP_
-#define NANO_ENGINE_SYSTEMS_DISPLAY_DISPLAY_HPP_
+#ifndef NANO_ENGINE_SYSTEMS_DISPLAY_DISPLAY_SYSTEM_HPP_
+#define NANO_ENGINE_SYSTEMS_DISPLAY_DISPLAY_SYSTEM_HPP_
 
+#include <array>
+#include <cstddef>
 #include <memory>
 #include <stdexcept>
+#include <string>
 #include <vector>
 
 #include <SDL2/SDL.h>
@@ -16,6 +19,51 @@ namespace ne
 class display_system : public system
 {
 public:
+  struct display_mode
+  {
+    explicit display_mode(const SDL_DisplayMode& native) 
+    : format      (native.format)
+    , size        ({static_cast<std::size_t>(native.w), static_cast<std::size_t>(native.h)})
+    , refresh_rate( static_cast<std::size_t>(native.refresh_rate))
+    {
+
+    }
+    display_mode ()                                    = default;
+    display_mode (const display_mode&  that)           = default;
+    display_mode (      display_mode&& temp)           = default;
+    ~display_mode()                                    = default;
+    display_mode& operator=(const display_mode&  that) = default;
+    display_mode& operator=(      display_mode&& temp) = default;
+    
+    SDL_DisplayMode to_native() const
+    {
+      return {format, static_cast<int>(size[0]), static_cast<int>(size[1]), static_cast<int>(refresh_rate), nullptr};
+    }
+
+    std::uint32_t              format      ;
+    std::array<std::size_t, 2> size        ;
+    std::size_t                refresh_rate;
+  };
+  struct display_info
+  {
+    display_mode closest_display_mode(const display_mode& mode) const
+    {
+      auto native_mode = mode.to_native();
+      SDL_DisplayMode native_closest_mode;
+      SDL_GetClosestDisplayMode(static_cast<int>(index), &native_mode, &native_closest_mode);
+      return display_mode(native_closest_mode);
+    }
+
+    std::size_t                index               ;
+    std::string                name                ;
+    std::array<std::size_t, 2> size                ;
+    std::array<std::size_t, 2> usable_size         ;
+    std::array<float      , 3> dpi                 ; // width, height, diagonal
+    std::vector<display_mode>  display_modes       ;
+    display_mode               current_display_mode;
+    display_mode               desktop_display_mode;
+  };
+
   display_system           ()
   {
     if (SDL_VideoInit(nullptr) != 0)
@@ -30,18 +78,29 @@ public:
   display_system& operator=(const display_system&  that) = default;
   display_system& operator=(      display_system&& temp) = default;
 
-  window*        create_window       ()
+  // Factory functionality.
+  enum class message_level : unsigned
   {
-    windows_.push_back(std::make_unique<window>());
+    error       = SDL_MESSAGEBOX_ERROR,
+    warning     = SDL_MESSAGEBOX_WARNING,
+    information = SDL_MESSAGEBOX_INFORMATION
+  };
+  static void create_message_box(std::string title, std::string message, message_level level = message_level::information)
+  {
+    SDL_ShowSimpleMessageBox(static_cast<Uint32>(level), title.c_str(), message.c_str(), nullptr);
+  }
+
+  window*                     create_window        ()
+  {
+    windows_.emplace_back(std::make_unique<window>());
     return windows_.back().get();
   }
-  opengl_window* create_opengl_window(const opengl_window::context_settings& context_settings = opengl_window::context_settings())
+  opengl_window*              create_opengl_window (const opengl_window::context_settings& context_settings = opengl_window::context_settings())
   {
-    opengl_windows_.push_back(std::make_unique<opengl_window>(context_settings));
+    opengl_windows_.emplace_back(std::make_unique<opengl_window>(context_settings));
     return opengl_windows_.back().get();
   }
-  
-  void destroy_window       (window*        window       )
+  void                        destroy_window       (window*        window       )
   {
     windows_.erase(std::remove_if(
       windows_.begin(), 
@@ -52,7 +111,7 @@ public:
       }), 
       windows_.end  ());
   }
-  void destroy_opengl_window(opengl_window* opengl_window)
+  void                        destroy_opengl_window(opengl_window* opengl_window)
   {
     opengl_windows_.erase(std::remove_if(
       opengl_windows_.begin(),
@@ -63,8 +122,7 @@ public:
       }), 
       opengl_windows_.end  ());
   }
-
-  std::vector<window*>        get_windows       () const
+  std::vector<window*>        windows              () const
   {
     std::vector<window*> windows(windows_.size());
     std::transform(
@@ -77,7 +135,7 @@ public:
       });
     return windows;
   }
-  std::vector<opengl_window*> get_opengl_windows() const
+  std::vector<opengl_window*> opengl_windows       () const
   {
     std::vector<opengl_window*> windows(opengl_windows_.size());
     std::transform(
@@ -89,6 +147,66 @@ public:
         return window.get();
       });
     return windows;
+  }
+
+  // Auxiliary functionality.
+  static std::string               current_video_driver   ()
+  {
+    return std::string(SDL_GetCurrentVideoDriver());
+  }
+  static std::vector<std::string>  video_drivers          ()
+  {
+    std::vector<std::string> drivers;
+    for(auto i = 0; i < SDL_GetNumVideoDrivers(); ++i)
+      drivers.emplace_back(SDL_GetVideoDriver(i));
+    return drivers;
+  }
+  static std::vector<display_info> displays               ()
+  {
+    std::vector<display_info> displays;
+    for(auto i = 0; i < SDL_GetNumVideoDisplays(); ++i)
+    {
+      auto name = std::string(SDL_GetDisplayName(i));
+
+      SDL_Rect bounds, usable_bounds;
+      SDL_GetDisplayBounds      (i, &bounds       );
+      SDL_GetDisplayUsableBounds(i, &usable_bounds);
+
+      std::array<float, 3> dpi;
+      SDL_GetDisplayDPI(i, &dpi[2], &dpi[0], &dpi[1]);
+      
+      std::vector<display_mode> display_modes;
+      for(auto j = 0; j < SDL_GetNumDisplayModes(i); ++j)
+      {
+        SDL_DisplayMode native_display_mode;
+        SDL_GetDisplayMode(i, j, &native_display_mode);
+        display_modes.emplace_back(native_display_mode);
+      }
+
+      SDL_DisplayMode native_current_display_mode, native_desktop_display_mode;
+      SDL_GetCurrentDisplayMode(i, &native_current_display_mode);
+      SDL_GetDesktopDisplayMode(i, &native_desktop_display_mode);
+
+      displays.push_back({
+        static_cast<std::size_t>(i),
+        name,
+        {static_cast<std::size_t>(bounds       .w), static_cast<std::size_t>(bounds       .h)},
+        {static_cast<std::size_t>(usable_bounds.w), static_cast<std::size_t>(usable_bounds.h)},
+        dpi,
+        display_modes,
+        display_mode(native_current_display_mode),
+        display_mode(native_desktop_display_mode)
+      });
+    }
+    return displays;
+  }
+  static void                      set_screensaver_enabled(bool enabled)
+  {
+    enabled ? SDL_EnableScreenSaver() : SDL_DisableScreenSaver();
+  }
+  static bool                      screensaver_enabled    ()
+  {
+    return SDL_IsScreenSaverEnabled() != 0;
   }
 
 protected:
