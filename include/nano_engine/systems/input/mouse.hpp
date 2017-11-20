2e3b46ef1c9abe12dc3521458d577c4266c0ee70
demiralp <demiralp@vr.rwth-aachen.de>
2017-11-20 12:24:40 +0100

    Mouse input implementation progress.

--- include/nano_engine/systems/input/mouse.hpp
+++ include/nano_engine/systems/input/mouse.hpp
@@ -1,11 +1,58 @@
 #ifndef NANO_ENGINE_SYSTEMS_INPUT_MOUSE_HPP_
 #define NANO_ENGINE_SYSTEMS_INPUT_MOUSE_HPP_
 
+#include <array>
+#include <cstddef>
+
 #include <SDL2/SDL_mouse.h>
 
 namespace ne
 {
+namespace mouse
+{
+inline void                       set_visible          (bool visible )
+{
+  SDL_ShowCursor(static_cast<int>(visible));
+}
+inline void                       set_captured         (bool captured)
+{
+  SDL_CaptureMouse(static_cast<SDL_bool>(captured));
+}
 
+inline void                       set_relative         (bool relative)
+{
+  SDL_SetRelativeMouseMode(static_cast<SDL_bool>(relative));
+}
+inline bool                       relative             ()
+{
+  return static_cast<bool>(SDL_GetRelativeMouseMode());
+}
+
+inline void                       set_absolute_position(const std::array<std::size_t, 2>& position)
+{
+  SDL_WarpMouseGlobal(static_cast<int>(position[0]), static_cast<int>(position[1]));
+}
+inline std::array<std::size_t, 2> absolute_position    ()
+{
+  std::array<std::size_t, 2> position;
+  SDL_GetGlobalMouseState(reinterpret_cast<int*>(&position[0]), reinterpret_cast<int*>(&position[1]));
+  return position;
+}
+
+inline std::array<std::size_t, 2> relative_position    (bool delta_only = false)
+{
+  std::array<std::size_t, 2> position;
+  delta_only ? 
+    SDL_GetRelativeMouseState(reinterpret_cast<int*>(&position[0]), reinterpret_cast<int*>(&position[1])): 
+    SDL_GetMouseState        (reinterpret_cast<int*>(&position[0]), reinterpret_cast<int*>(&position[1]));
+  return position;
+}
+
+inline bool                       is_button_pressed    (std::size_t button)
+{
+  return SDL_GetGlobalMouseState(nullptr, nullptr) & static_cast<Uint32>(button);
+}
+}
 }
 
 #endif
