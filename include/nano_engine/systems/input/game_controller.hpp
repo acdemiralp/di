1af44848f7e7ffeae140df5369484ecdb29bf847
Demiralp <demiralpali@gmail.com>
2017-11-23 20:13:35 +0100

    Game controller integration progress.

--- include/nano_engine/systems/input/game_controller.hpp
+++ include/nano_engine/systems/input/game_controller.hpp
@@ -1,11 +1,33 @@
 #ifndef NANO_ENGINE_SYSTEMS_INPUT_GAME_CONTROLLER_HPP_
 #define NANO_ENGINE_SYSTEMS_INPUT_GAME_CONTROLLER_HPP_
 
+#include <cstddef>
+#include <stdexcept>
+
 #include <SDL2/SDL_gamecontroller.h>
 
 namespace ne
 {
+class game_controller
+{
+public:
+  game_controller           (std::size_t index) : native_(SDL_GameControllerOpen(index))
+  {
+    if(!native_)
+      throw std::runtime_error("Failed to create SDL game controller. SDL Error: " + std::string(SDL_GetError()));
+  }
+  game_controller           (const game_controller&  that) = delete ;
+  game_controller           (      game_controller&& temp) = default;
+  ~game_controller          ()
+  {
+    SDL_GameControllerClose(native_);
+  }
+  game_controller& operator=(const game_controller&  that) = delete ;
+  game_controller& operator=(      game_controller&& temp) = default;
 
+private:
+  SDL_GameController* native_;
+};
 }
 
 #endif
