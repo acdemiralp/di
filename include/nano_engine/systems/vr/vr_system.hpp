41fdca6fc46d9bb080d098a5433f3ba9f0e10610
Demiralp <demiralpali@gmail.com>
2017-11-27 23:09:30 +0100

    VR system implementation progress.

--- include/nano_engine/systems/vr/vr_system.hpp
+++ include/nano_engine/systems/vr/vr_system.hpp
@@ -36,7 +36,7 @@ public:
   vr_system& operator=(const vr_system&  that) = delete ;
   vr_system& operator=(      vr_system&& temp) = default;
 
-  std::array<std::size_t, 2> recommended_render_target_size() const
+  std::array<std::size_t, 2>          recommended_render_target_size() const
   {
     std::array<std::size_t, 2> size;
     native_->GetRecommendedRenderTargetSize(
@@ -44,16 +44,32 @@ public:
       reinterpret_cast<std::uint32_t*>(&size[1]));
     return size;
   }
+  std::array<std::array<float, 16>,2> projection_matrix             (float z_near, float z_far) const
+  {
+    std::array<std::array<float, 16>, 2> matrix;
+    auto left_eye_matrix  = native_->GetProjectionMatrix(vr::EVREye::Eye_Left , z_near, z_far);
+    auto right_eye_matrix = native_->GetProjectionMatrix(vr::EVREye::Eye_Right, z_near, z_far);
+    std::copy(&left_eye_matrix [0][0], &left_eye_matrix [0][0] + 16, &matrix[0]);
+    std::copy(&right_eye_matrix[0][0], &right_eye_matrix[0][0] + 16, &matrix[1]);
+    return matrix;
+  }
+  std::array<std::array<float, 4>, 2> projection_parameters         () const
+  {
+    std::array<std::array<float, 4>, 2> parameters;
+    native_->GetProjectionRaw(vr::EVREye::Eye_Left , &parameters[0][0], &parameters[0][1], &parameters[0][2], &parameters[0][3]);
+    native_->GetProjectionRaw(vr::EVREye::Eye_Right, &parameters[1][0], &parameters[1][1], &parameters[1][2], &parameters[1][3]);
+    return parameters;
+  }
 
-  static bool                hardware_present              ()
+  static bool                         hardware_present              ()
   {
     return vr::VR_IsHmdPresent();
   }
-  static bool                runtime_installed             ()
+  static bool                         runtime_installed             ()
   {
     return vr::VR_IsRuntimeInstalled();
   }
-  static std::string         runtime_location              ()
+  static std::string                  runtime_location              ()
   {
     return std::string(vr::VR_RuntimePath());
   }
