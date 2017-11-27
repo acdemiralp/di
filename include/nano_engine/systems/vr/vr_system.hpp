9b6a58f124200e7bb5c42d99310152addb280740
Demiralp <demiralpali@gmail.com>
2017-11-27 01:12:25 +0100

    VR system implementation progress.

--- include/nano_engine/systems/vr/vr_system.hpp
+++ include/nano_engine/systems/vr/vr_system.hpp
@@ -1,6 +1,10 @@
 #ifndef NANO_ENGINE_SYSTEMS_VR_VR_SYSTEM_HPP_
 #define NANO_ENGINE_SYSTEMS_VR_VR_SYSTEM_HPP_
 
+#include <array>
+#include <cstddef>
+#include <stdexcept>
+
 #include <openvr.h>
 
 #include <nano_engine/system.hpp>
@@ -10,34 +14,55 @@ namespace ne
 class vr_system final : public system
 {
 public:
-  vr_system           ()
+  vr_system           () 
+  : native_       (VR_Init(&init_error_, vr::VRApplication_Scene))
+  , chaperone_    (static_cast<vr::IVRChaperone*>   (VR_GetGenericInterface(vr::IVRChaperone_Version   , &interface_error_)))
+  , compositor_   (static_cast<vr::IVRCompositor*>  (VR_GetGenericInterface(vr::IVRCompositor_Version  , &interface_error_)))
+  , overlay_      (static_cast<vr::IVROverlay*>     (VR_GetGenericInterface(vr::IVROverlay_Version     , &interface_error_)))
+  , render_models_(static_cast<vr::IVRRenderModels*>(VR_GetGenericInterface(vr::IVRRenderModels_Version, &interface_error_)))
+  , screenshots_  (static_cast<vr::IVRScreenshots*> (VR_GetGenericInterface(vr::IVRScreenshots_Version , &interface_error_)))
   {
-    auto error    = vr::VRInitError_None;
-    vr_interface_ = vr::VR_Init(&error, vr::VRApplication_Scene);
-    if  (error   != vr::VRInitError_None)
-    {
-      // Unable to find HMD. Proceed without.
-    }
-    auto chaperone     = static_cast<vr::IVRChaperone*>   (VR_GetGenericInterface(vr::IVRChaperone_Version   , &error));
-    auto compositor    = static_cast<vr::IVRCompositor*>  (VR_GetGenericInterface(vr::IVRCompositor_Version  , &error));
-    auto overlay       = static_cast<vr::IVROverlay*>     (VR_GetGenericInterface(vr::IVROverlay_Version     , &error));
-    auto render_models = static_cast<vr::IVRRenderModels*>(VR_GetGenericInterface(vr::IVRRenderModels_Version, &error));
-    auto screenshots   = static_cast<vr::IVRScreenshots*> (VR_GetGenericInterface(vr::IVRScreenshots_Version , &error));
+    if (init_error_      != vr::VRInitError_None)
+      throw std::runtime_error("Failed to initialize OpenVR. Error: "          + std::string(VR_GetVRInitErrorAsSymbol(init_error_     )));
+    if (interface_error_ != vr::VRInitError_None)
+      throw std::runtime_error("Failed to retrieve OpenVR interfaces. Error: " + std::string(VR_GetVRInitErrorAsSymbol(interface_error_)));
   }
-  vr_system           (const vr_system&  that) = default;
+  vr_system           (const vr_system&  that) = delete ;
   vr_system           (      vr_system&& temp) = default;
   virtual ~vr_system  ()
   {
     vr::VR_Shutdown();
   }
-  vr_system& operator=(const vr_system&  that) = default;
+  vr_system& operator=(const vr_system&  that) = delete ;
   vr_system& operator=(      vr_system&& temp) = default;
+
+  std::array<std::size_t, 2> recommended_render_target_size() const
+  {
+    std::array<std::size_t, 2> size;
+    native_->GetRecommendedRenderTargetSize(
+      reinterpret_cast<std::uint32_t*>(&size[0]), 
+      reinterpret_cast<std::uint32_t*>(&size[1]));
+    return size;
+  }
+
+  static bool                hardware_present              ()
+  {
+    return vr::VR_IsHmdPresent();
+  }
+  static bool                runtime_installed             ()
+  {
+    return vr::VR_IsRuntimeInstalled();
+  }
+  static std::string         runtime_location              ()
+  {
+    return std::string(vr::VR_RuntimePath());
+  }
 
 private:
   void tick() override
   {
     vr::VREvent_t event;
-    while (vr_interface_->PollNextEvent(&event, sizeof event))
+    while (native_->PollNextEvent(&event, sizeof event))
     {
       if      (event.eventType == vr::VREvent_TrackedDeviceActivated  )
       {
@@ -54,7 +79,14 @@ private:
     }
   }
 
-  vr::IVRSystem* vr_interface_;
+  vr::EVRInitError     init_error_      = vr::VRInitError_None;
+  vr::EVRInitError     interface_error_ = vr::VRInitError_None;
+  vr::IVRSystem*       native_          = nullptr;
+  vr::IVRChaperone*    chaperone_       = nullptr;
+  vr::IVRCompositor*   compositor_      = nullptr;
+  vr::IVROverlay*      overlay_         = nullptr;
+  vr::IVRRenderModels* render_models_   = nullptr;
+  vr::IVRScreenshots*  screenshots_     = nullptr;
 };
 }
 
