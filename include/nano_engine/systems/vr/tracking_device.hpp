3d22714b62a8a16e29dbe3060cc1e6c9d42f055b
Demiralp <demiralpali@gmail.com>
2017-12-07 00:40:21 +0100

    Added hand, tracking device, tracking mode.

---
+++ include/nano_engine/systems/vr/tracking_device.hpp
@@ -0,0 +1,19 @@
+#ifndef NANO_ENGINE_SYSTEMS_VR_TRACKING_DEVICE_HPP_
+#define NANO_ENGINE_SYSTEMS_VR_TRACKING_DEVICE_HPP_
+
+#include <openvr.h>
+
+namespace ne
+{
+enum class tracking_device
+{
+  invalid            = vr::TrackedDeviceClass_Invalid          ,
+  hmd                = vr::TrackedDeviceClass_HMD              ,
+  controller         = vr::TrackedDeviceClass_Controller       ,
+  generic_tracker    = vr::TrackedDeviceClass_GenericTracker   ,
+  tracking_reference = vr::TrackedDeviceClass_TrackingReference,
+  display_redirect   = vr::TrackedDeviceClass_DisplayRedirect
+};
+}
+
+#endif
---
+++ include/nano_engine/systems/vr/tracking_mode.hpp
@@ -0,0 +1,16 @@
+#ifndef NANO_ENGINE_SYSTEMS_VR_TRACKING_MODE_HPP_
+#define NANO_ENGINE_SYSTEMS_VR_TRACKING_MODE_HPP_
+
+#include <openvr.h>
+
+namespace ne
+{
+enum class tracking_mode
+{
+  seated       = vr::ETrackingUniverseOrigin::TrackingUniverseSeated  ,
+  standing     = vr::ETrackingUniverseOrigin::TrackingUniverseStanding,
+  uncalibrated = vr::ETrackingUniverseOrigin::TrackingUniverseRawAndUncalibrated
+};
+}
+
+#endif
