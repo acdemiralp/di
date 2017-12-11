135c52fbaa86327065254dfdcd5a887b67c36958
Demiralp <demiralpali@gmail.com>
2017-12-11 21:16:36 +0100

    VR controller now uses a map keyed by axis type to store the axes, rather than an array.

--- include/nano_engine/systems/vr/vr_controller_state.hpp
+++ include/nano_engine/systems/vr/vr_controller_state.hpp
@@ -13,12 +13,15 @@ namespace ne
 {
 struct vr_controller_state : tracking_device_pose
 {
-  vr_controller_state           (const vr::TrackedDevicePose_t& native_pose, const vr::VRControllerState_t& native_state) 
+  vr_controller_state           (
+    const vr::TrackedDevicePose_t& native_pose, 
+    const vr::VRControllerState_t& native_state, 
+    const std::array<vr_controller_axis, vr::k_unControllerStateAxisCount>& axis_types)
   : tracking_device_pose(native_pose)
   {
     const auto native_axes = native_state.rAxis;
     for (auto i = 0; i < axes.size(); ++i)
-      axes[i] = {native_axes[i].x, native_axes[i].y};
+      axes[axis_types[i]] = {native_axes[i].x, native_axes[i].y};
 
     const auto pressed = native_state.ulButtonPressed;
     const auto touched = native_state.ulButtonTouched;
@@ -37,8 +40,8 @@ struct vr_controller_state : tracking_device_pose
   vr_controller_state& operator=(const vr_controller_state&  that) = default;
   vr_controller_state& operator=(      vr_controller_state&& temp) = default;
 
-  std::array<std::array<float, 2>, vr::k_unControllerStateAxisCount> axes   ;
-  std::map<vr_controller_button, vr_controller_button_state>         buttons;
+  std::map<vr_controller_axis  , std::array<float, 2>>       axes   ;
+  std::map<vr_controller_button, vr_controller_button_state> buttons;
 };
 }
 
