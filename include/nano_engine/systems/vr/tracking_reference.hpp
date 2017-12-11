a42d428cc1dea31cd1087bbc3f2e09af3a13dfc6
Demiralp <demiralpali@gmail.com>
2017-12-11 21:33:42 +0100

    Property implementations complete.

--- include/nano_engine/systems/vr/tracking_reference.hpp
+++ include/nano_engine/systems/vr/tracking_reference.hpp
@@ -1,7 +1,10 @@
 #ifndef NANO_ENGINE_SYSTEMS_VR_TRACKING_REFERENCE_HPP_
 #define NANO_ENGINE_SYSTEMS_VR_TRACKING_REFERENCE_HPP_
 
+#include <openvr.h>
+
 #include <nano_engine/systems/vr/tracking_device.hpp>
+#include <nano_engine/utility/rectangle.hpp>
 
 namespace ne
 {
@@ -17,6 +20,30 @@ public:
   virtual ~tracking_reference  ()                                = default;
   tracking_reference& operator=(const tracking_reference&  that) = default;
   tracking_reference& operator=(      tracking_reference&& temp) = default;
+
+  // IVR System - Property
+  rectangle<float>     field_of_view () const
+  {
+    return rectangle<float>  
+    {
+      get_property_float(vr::Prop_FieldOfViewLeftDegrees_Float  ),
+      get_property_float(vr::Prop_FieldOfViewRightDegrees_Float ),
+      get_property_float(vr::Prop_FieldOfViewTopDegrees_Float   ),
+      get_property_float(vr::Prop_FieldOfViewBottomDegrees_Float)
+    };
+  }
+  std::array<float, 2> tracking_range() const
+  {
+    return std::array<float, 2> 
+    {
+      get_property_float(vr::Prop_TrackingRangeMinimumMeters_Float),
+      get_property_float(vr::Prop_TrackingRangeMaximumMeters_Float)
+    };
+  }
+  std::string          mode_label    () const
+  {
+    return get_property_string(vr::Prop_ModeLabel_String);
+  }
 };
 }
 
