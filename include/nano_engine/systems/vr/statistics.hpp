8d406de12a5279e238053463b945925d90bfa271
Demiralp <demiralpali@gmail.com>
2017-12-30 19:19:04 +0100

    Formatting correction.

--- include/nano_engine/systems/vr/statistics.hpp
+++ include/nano_engine/systems/vr/statistics.hpp
@@ -7,25 +7,25 @@ namespace ne
 {
 struct statistics
 {
-	std::size_t process_id                    ;
+  std::size_t process_id                    ;
 
-	std::size_t present_count                 ;
-	std::size_t drop_count                    ;
-	std::size_t reprojected_drop_count        ;
+  std::size_t present_count                 ;
+  std::size_t drop_count                    ;
+  std::size_t reprojected_drop_count        ;
 
-	std::size_t startup_present_count         ;
-	std::size_t startup_drop_count            ;
-	std::size_t startup_reprojected_drop_count;
+  std::size_t startup_present_count         ;
+  std::size_t startup_drop_count            ;
+  std::size_t startup_reprojected_drop_count;
 
-	std::size_t loading_count                 ;
-	std::size_t loading_present_count         ;
-	std::size_t loading_drop_count            ;
-	std::size_t loading_reprojected_drop_count;
+  std::size_t loading_count                 ;
+  std::size_t loading_present_count         ;
+  std::size_t loading_drop_count            ;
+  std::size_t loading_reprojected_drop_count;
 
-	std::size_t timeout_count                 ;
-	std::size_t timeout_present_count         ;
+  std::size_t timeout_count                 ;
+  std::size_t timeout_present_count         ;
   std::size_t timeout_drop_count            ;
-	std::size_t timeout_reprojected_drop_count;
+  std::size_t timeout_reprojected_drop_count;
 };
 }
 
