a508c552bee5f2ee7951607c839030dd5d1a66b5
Demiralp <demiralpali@gmail.com>
2017-12-30 19:28:11 +0100

    Tracking device model integration progress.

--- include/nano_engine/systems/vr/tracking_device.hpp
+++ include/nano_engine/systems/vr/tracking_device.hpp
@@ -3,6 +3,7 @@
 
 #include <algorithm>
 #include <cstdint>
+#include <memory>
 #include <string>
 
 #include <boost/optional.hpp>
@@ -10,6 +11,7 @@
 
 #include <nano_engine/systems/vr/hand.hpp>
 #include <nano_engine/systems/vr/tracking_device_activity.hpp>
+#include <nano_engine/systems/vr/tracking_device_model.hpp>
 #include <nano_engine/systems/vr/tracking_device_pose.hpp>
 #include <nano_engine/systems/vr/tracking_device_type.hpp>
 
@@ -33,199 +35,202 @@ public:
   tracking_device& operator=(      tracking_device&& temp) = default;
   
   // IVR System - Tracking
-  tracking_device_activity activity                        () const
+  tracking_device_activity               activity                        () const
   {
     return static_cast<tracking_device_activity>(vr::VRSystem()->GetTrackedDeviceActivityLevel(index_));
   }
-  hand                     hand                            () const
+  hand                                   hand                            () const
   {
     return static_cast<ne::hand>(vr::VRSystem()->GetControllerRoleForTrackedDeviceIndex(index_));
   }
                                                               
   // IVR System - Property                                    
-  bool                     connected                       () const
+  bool                                   connected                       () const
   {
     return vr::VRSystem()->IsTrackedDeviceConnected(index_);
   }
-
-  std::array<float, 12>    display_transform               () const
+                                         
+  std::array<float, 12>                  display_transform               () const
   {
     return get_property_matrix34(vr::Prop_StatusDisplayTransform_Matrix34);
   }
-  
-  std::string              resource_root                   () const
+                                         
+  std::string                            resource_root                   () const
   {
     return get_property_string(vr::Prop_ResourceRoot_String);
   }
-  std::string              render_model_name               () const
+  std::string                            render_model_name               () const
   {
     return get_property_string(vr::Prop_RenderModelName_String);
   }
-
-  bool                     can_power_off                   () const
+                                         
+  bool                                   can_power_off                   () const
   {
     return get_property_bool(vr::Prop_DeviceCanPowerOff_Bool);
   }
-  bool                     is_charging                     () const
+  bool                                   is_charging                     () const
   {
     return get_property_bool(vr::Prop_DeviceIsCharging_Bool);
   }
-  bool                     has_battery_status              () const
+  bool                                   has_battery_status              () const
   {
     return get_property_bool(vr::Prop_DeviceProvidesBatteryStatus_Bool);
   }
-  float                    battery_percentage              () const
+  float                                  battery_percentage              () const
   {
     return get_property_float(vr::Prop_DeviceBatteryPercentage_Float);
   }
-  
-  bool                     is_wireless                     () const
+                                         
+  bool                                   is_wireless                     () const
   {
     return get_property_bool(vr::Prop_DeviceIsWireless_Bool);
   }
-  std::uint64_t            dongle_version                  () const
+  std::uint64_t                          dongle_version                  () const
   {
     return get_property_uint64(vr::Prop_DongleVersion_Uint64);
   }
-  std::string              dongle_descriptions             () const
+  std::string                            dongle_descriptions             () const
   {
     return get_property_string(vr::Prop_AllWirelessDongleDescriptions_String);
   }
-  std::string              connected_dongle                () const
+  std::string                            connected_dongle                () const
   {
     return get_property_string(vr::Prop_ConnectedWirelessDongle_String);
   }
-  
-  bool                     has_yaw_drift                   () const
+                                         
+  bool                                   has_yaw_drift                   () const
   {
     return get_property_bool(vr::Prop_WillDriftInYaw_Bool);
   }
-  bool                     has_proximity_sensor            () const
+  bool                                   has_proximity_sensor            () const
   {
     return get_property_bool(vr::Prop_ContainsProximitySensor_Bool);
   }
-  bool                     has_camera                      () const
+  bool                                   has_camera                      () const
   {
     return get_property_bool(vr::Prop_HasCamera_Bool);
   }
-  bool                     blocks_server_shutdown          () const
+  bool                                   blocks_server_shutdown          () const
   {
     return get_property_bool(vr::Prop_BlockServerShutdown_Bool);
   }
-  bool                     can_unify_coordinates_with_hmd  () const
+  bool                                   can_unify_coordinates_with_hmd  () const
   {
     return get_property_bool(vr::Prop_CanUnifyCoordinateSystemWithHmd_Bool);
   }
-  
-  std::string              manufacturer_name               () const
+                                         
+  std::string                            manufacturer_name               () const
   {
     return get_property_string(vr::Prop_ManufacturerName_String);
   }
-  std::string              tracking_system_name            () const
+  std::string                            tracking_system_name            () const
   {
     return get_property_string(vr::Prop_TrackingSystemName_String);
   }
-  std::string              model_number                    () const
+  std::string                            model_number                    () const
   {
     return get_property_string(vr::Prop_ModelNumber_String);
   }
-  std::string              serial_number                   () const
+  std::string                            serial_number                   () const
   {
     return get_property_string(vr::Prop_SerialNumber_String);
   }
- 
-  std::uint64_t            hardware_revision               () const
+                                         
+  std::uint64_t                          hardware_revision               () const
   {
     return get_property_uint64(vr::Prop_HardwareRevision_Uint64);
   }
-  std::string              hardware_revision_string        () const
+  std::string                            hardware_revision_string        () const
   {
     return get_property_string(vr::Prop_HardwareRevision_String);
   }
-  
-  std::uint64_t            parent_driver                   () const
+                                         
+  std::uint64_t                          parent_driver                   () const
   {
     return get_property_uint64(vr::Prop_ParentDriver_Uint64);
   }
-  std::string              driver_version                  () const
+  std::string                            driver_version                  () const
   {
     return get_property_string(vr::Prop_DriverVersion_String);
   }
-  
-  std::uint64_t            firmware_version                () const
+                                         
+  std::uint64_t                          firmware_version                () const
   {
     return get_property_uint64(vr::Prop_FirmwareVersion_Uint64);
   }
-  std::string              firmware_version_string         () const
+  std::string                            firmware_version_string         () const
   {
     return get_property_string(vr::Prop_TrackingFirmwareVersion_String);
   }
-  std::string              firmware_programming_target     () const
+  std::string                            firmware_programming_target     () const
   {
     return get_property_string(vr::Prop_Firmware_ProgrammingTarget_String);
   }
-  bool                     firmware_update_available       () const
+  bool                                   firmware_update_available       () const
   {
     return get_property_bool(vr::Prop_Firmware_UpdateAvailable_Bool);
   }
-  bool                     firmware_update_required        () const
+  bool                                   firmware_update_required        () const
   {
     return get_property_bool(vr::Prop_Firmware_ForceUpdateRequired_Bool);
   }
-  bool                     firmware_manual_update          () const
+  bool                                   firmware_manual_update          () const
   {
     return get_property_bool(vr::Prop_Firmware_ManualUpdate_Bool);
   }
-  std::string              firmware_manual_update_url      () const
+  std::string                            firmware_manual_update_url      () const
   {
     return get_property_string(vr::Prop_Firmware_ManualUpdateURL_String);
   }
-  
-  std::uint64_t            fpga_version                    () const
+                                         
+  std::uint64_t                          fpga_version                    () const
   {
     return get_property_uint64(vr::Prop_FPGAVersion_Uint64);
   }
-  std::uint64_t            vrc_version                     () const
+  std::uint64_t                          vrc_version                     () const
   {
     return get_property_uint64(vr::Prop_VRCVersion_Uint64);
   }
-  std::uint64_t            radio_version                   () const
+  std::uint64_t                          radio_version                   () const
   {
     return get_property_uint64(vr::Prop_RadioVersion_Uint64);
   }
-
-  std::string              registered_device_type          () const
+                                         
+  std::string                            registered_device_type          () const
   {
     return get_property_string(vr::Prop_RegisteredDeviceType_String);
   }
-  std::string              input_profile_name              () const
+  std::string                            input_profile_name              () const
   {
     return get_property_string(vr::Prop_InputProfileName_String);
   }
-  
-  // IVR System - Debug                                                                      
-  std::string              send_driver_request             (const std::string& request) const
+                                         
+  // IVR System - Debug                                                                                    
+  std::string                            send_driver_request             (const std::string& request) const
   {
     char raw_response[32768];
     vr::VRSystem()->DriverDebugRequest(index_, request.data(), &raw_response[0], 32768);
     return std::string(raw_response);
   }
-                                                                                                        
-  // IVR System - Firmware                                                                              
-  void                     update_firmware                 () const
+                                                                                                                      
+  // IVR System - Firmware                                                                                            
+  void                                   update_firmware                 () const
   {
     vr::VRSystem()->PerformFirmwareUpdate(index_);
   }
   
   // IVR System - Render Models
-  // TODO
-
+  std::unique_ptr<tracking_device_model> model                           () const
+  {
+    return std::make_unique<tracking_device_model>(render_model_name());
+  }
+  
   // Auxiliary
-  std::uint32_t            index                           () const
+  std::uint32_t                          index                           () const
   {
     return index_;
   }
-  tracking_device_pose*    pose                            ()
+  tracking_device_pose*                  pose                            ()
   {
     return pose_.is_initialized() ? pose_.get_ptr() : nullptr;
   }
@@ -233,35 +238,35 @@ public:
 protected:
   friend vr_system;
 
-  // IVR System - Property                                 
-  bool                     get_property_bool               (const vr::ETrackedDeviceProperty native_property) const
+  // IVR System - Property                                               
+  bool                                   get_property_bool               (const vr::ETrackedDeviceProperty native_property) const
   {
     auto error = vr::TrackedProp_Success;
     return vr::VRSystem()->GetBoolTrackedDeviceProperty  (index_, native_property, &error);
   }
-  float                    get_property_float              (const vr::ETrackedDeviceProperty native_property) const
+  float                                  get_property_float              (const vr::ETrackedDeviceProperty native_property) const
   {
     auto error = vr::TrackedProp_Success;
     return vr::VRSystem()->GetFloatTrackedDeviceProperty (index_, native_property, &error);
   }
-  int                      get_property_int                (const vr::ETrackedDeviceProperty native_property) const
+  int                                    get_property_int                (const vr::ETrackedDeviceProperty native_property) const
   {
     auto error = vr::TrackedProp_Success;
     return vr::VRSystem()->GetInt32TrackedDeviceProperty (index_, native_property, &error);
   }
-  std::uint64_t            get_property_uint64             (const vr::ETrackedDeviceProperty native_property) const
+  std::uint64_t                          get_property_uint64             (const vr::ETrackedDeviceProperty native_property) const
   {
     auto error = vr::TrackedProp_Success;
     return vr::VRSystem()->GetUint64TrackedDeviceProperty(index_, native_property, &error);
   }
-  std::array<float, 12>    get_property_matrix34           (const vr::ETrackedDeviceProperty native_property) const
+  std::array<float, 12>                  get_property_matrix34           (const vr::ETrackedDeviceProperty native_property) const
   {
     auto error = vr::TrackedProp_Success;
     const auto native_matrix = vr::VRSystem()->GetMatrix34TrackedDeviceProperty(index_, native_property, &error);
     std::array<float, 12> matrix; std::copy(&native_matrix.m[0][0], &native_matrix.m[0][0] + 12, matrix.begin());
     return matrix;
   }
-  std::string              get_property_string             (const vr::ETrackedDeviceProperty native_property) const
+  std::string                            get_property_string             (const vr::ETrackedDeviceProperty native_property) const
   {
     auto error = vr::TrackedProp_Success;
     char native_string[vr::k_unMaxPropertyStringSize];
--- include/nano_engine/systems/vr/tracking_device_model.hpp
+++ include/nano_engine/systems/vr/tracking_device_model.hpp
@@ -1,15 +1,112 @@
 #ifndef NANO_ENGINE_SYSTEMS_VR_TRACKING_DEVICE_MODEL_HPP_
 #define NANO_ENGINE_SYSTEMS_VR_TRACKING_DEVICE_MODEL_HPP_
 
+#include <algorithm>
 #include <array>
+#include <cstddef>
+#include <memory>
+#include <stdexcept>
+#include <string>
 #include <vector>
 
 #include <nano_engine/systems/vr/tracking_device_texture.hpp>
+
+class ID3D11Device;
+class ID3D11Texture2D;
 
 namespace ne
 {
 struct tracking_device_model
 {
+  explicit tracking_device_model  (const std::string& name) : name(name)
+  {
+    vr::RenderModel_t*      render_model;
+    vr::EVRRenderModelError error       ;
+    do
+    {
+      error = vr::VRRenderModels()->LoadRenderModel_Async(name.c_str(), &render_model);
+    }
+    while (error == vr::VRRenderModelError_Loading);
+    if    (error != vr::VRRenderModelError_None   )
+      throw std::runtime_error("Failed to create OpenVR render model. OpenVR Error: " + std::string(vr::VRRenderModels()->GetRenderModelErrorNameFromEnum(error)));
+    
+    char native_original_path[vr::k_unMaxPropertyStringSize];
+    vr::VRRenderModels()->GetRenderModelOriginalPath(name.c_str(), native_original_path, vr::k_unMaxPropertyStringSize, &error);
+    original_path = native_original_path;
+
+    char native_thumbnail_url[vr::k_unMaxPropertyStringSize];
+    vr::VRRenderModels()->GetRenderModelThumbnailURL(name.c_str(), native_thumbnail_url, vr::k_unMaxPropertyStringSize, &error);
+    thumbnail_url = native_thumbnail_url;
+
+    vertices           .resize(static_cast<std::size_t>(    render_model->unVertexCount  ));
+    normals            .resize(static_cast<std::size_t>(    render_model->unVertexCount  ));
+    texture_coordinates.resize(static_cast<std::size_t>(    render_model->unVertexCount  ));
+    indices            .resize(static_cast<std::size_t>(3 * render_model->unTriangleCount));
+    std::transform(render_model->rVertexData, render_model->rVertexData + vertices           .size(), vertices           .begin(), [ ] (const vr::RenderModel_Vertex_t& iteratee)
+    {
+      return std::array<float, 3> {iteratee.vPosition.v[0], iteratee.vPosition.v[1], iteratee.vPosition.v[2]};
+    });
+    std::transform(render_model->rVertexData, render_model->rVertexData + normals            .size(), normals            .begin(), [ ] (const vr::RenderModel_Vertex_t& iteratee)
+    {
+      return std::array<float, 3> {iteratee.vNormal.v[0], iteratee.vNormal.v[1], iteratee.vNormal.v[2]};
+    });
+    std::transform(render_model->rVertexData, render_model->rVertexData + texture_coordinates.size(), texture_coordinates.begin(), [ ] (const vr::RenderModel_Vertex_t& iteratee)
+    {
+      return std::array<float, 2> {iteratee.rfTextureCoord[0], iteratee.rfTextureCoord[1]};
+    });
+    std::transform(render_model->rIndexData , render_model->rIndexData  + indices            .size(), indices            .begin(), [ ] (const std::uint16_t&            iteratee)
+    {
+      return static_cast<unsigned>(iteratee);
+    });
+    
+    texture_id = render_model->diffuseTextureId;
+
+    vr::VRRenderModels()->FreeRenderModel(render_model);
+  }
+  tracking_device_model           (const tracking_device_model&  that) = default;
+  tracking_device_model           (      tracking_device_model&& temp) = default;
+  virtual ~tracking_device_model  ()                                   = default;
+  tracking_device_model& operator=(const tracking_device_model&  that) = default;
+  tracking_device_model& operator=(      tracking_device_model&& temp) = default;
+
+  std::unique_ptr<tracking_device_texture> texture      ()                                                const
+  {
+    vr::RenderModel_TextureMap_t* native_texture;
+    vr::EVRRenderModelError       error         ;
+    do
+    {
+      error = vr::VRRenderModels()->LoadTexture_Async(texture_id, &native_texture);
+    }
+    while (error == vr::VRRenderModelError_Loading);
+    
+    auto texture  = std::make_unique<tracking_device_texture>();
+    texture->size = {static_cast<std::size_t>(native_texture->unWidth), static_cast<std::size_t>(native_texture->unHeight)};
+    texture->data.resize(4 * texture->size[0] * texture->size[1]);
+    std::copy(native_texture->rubTextureMapData, native_texture->rubTextureMapData + texture->data.size(), texture->data);
+
+    vr::VRRenderModels()->FreeTexture(native_texture);
+
+    return texture;
+  }
+  void                                     d3d11_texture(ID3D11Device* device, ID3D11Texture2D** texture) const
+  {
+    vr::EVRRenderModelError error;
+    do
+    {
+      error = vr::VRRenderModels()->LoadTextureD3D11_Async(texture_id, device, reinterpret_cast<void**>(texture));
+    }
+    while (error == vr::VRRenderModelError_Loading);
+  }
+  void                                     d3d11_texture(                      ID3D11Texture2D*  texture) const
+  {
+    vr::EVRRenderModelError error;
+    do
+    {
+      error = vr::VRRenderModels()->LoadIntoTextureD3D11_Async(texture_id, texture);
+    }
+    while (error == vr::VRRenderModelError_Loading);
+  }
+  
   std::string                       name               ;
   std::string                       original_path      ;
   std::string                       thumbnail_url      ;
@@ -17,7 +114,7 @@ struct tracking_device_model
   std::vector<std::array<float, 3>> normals            ;
   std::vector<std::array<float, 2>> texture_coordinates;
   std::vector<unsigned>             indices            ;
-  tracking_device_texture           diffuse_texture    ;
+  std::int32_t                      texture_id         ;
 };
 }
 
