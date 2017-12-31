0c0d911947803a216d69097cffb4c7fb07b1c501
Demiralp <demiralpali@gmail.com>
2018-01-01 00:12:19 +0100

    Overlay integration progress.

--- include/nano_engine/systems/vr/overlay.hpp
+++ include/nano_engine/systems/vr/overlay.hpp
@@ -23,10 +23,56 @@ public:
   }
   overlay& operator=(const overlay&  that) = default;
   overlay& operator=(      overlay&& temp) = default;
+  
+  std::string    key             ()                        const
+  {
+    char key[vr::k_unMaxPropertyStringSize];
+    vr::VROverlay()->GetOverlayKey(handle_, key, vr::k_unMaxPropertyStringSize);
+    return std::string(key);
+  }
+                 
+  void           set_name        (const std::string& name)
+  {
+    vr::VROverlay()->SetOverlayName(handle_, name.c_str());
+  }
+  std::string    name            ()                        const
+  {
+    char name[vr::k_unMaxPropertyStringSize];
+    vr::VROverlay()->GetOverlayName(handle_, name, vr::k_unMaxPropertyStringSize);
+    return std::string(name);
+  }
+            
+  void           set_visible     (bool visible)
+  {
+    visible ? vr::VROverlay()->ShowOverlay(handle_) : vr::VROverlay()->HideOverlay(handle_);
+  }
+  bool           visible         ()                        const
+  {
+    return vr::VROverlay()->IsOverlayVisible(handle_);
+  }
+
+  void           set_high_quality()
+  {
+    vr::VROverlay()->SetHighQualityOverlay(handle_);
+  }
+  bool           is_high_quality ()                        const
+  {
+    return vr::VROverlay()->GetHighQualityOverlay() == handle_;
+  }
+  
+  static overlay find            (const std::string& key)
+  {
+    std::uint64_t handle;
+    vr::VROverlay()->FindOverlay(key.c_str(), &handle);
+    return overlay(handle);
+  }
 
 protected:
-  overlay() : handle_(0) {}
+  explicit overlay(const std::uint64_t& handle = 0) : handle_(handle)
+  {
 
+  }
+  
   std::uint64_t handle_;
 };
 }
