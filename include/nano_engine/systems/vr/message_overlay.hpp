91639b6d0f3c787d5530f8354d7f347141ca2eb9
demiralp <demiralp@vr.rwth-aachen.de>
2018-01-05 15:21:57 +0100

    Added message overlay.

---
+++ include/nano_engine/systems/vr/message_overlay.hpp
@@ -0,0 +1,30 @@
+#ifndef NANO_ENGINE_SYSTEMS_VR_DASHBOARD_OVERLAY_HPP_
+#define NANO_ENGINE_SYSTEMS_VR_DASHBOARD_OVERLAY_HPP_
+
+#include <string>
+#include <vector>
+
+#include <openvr.h>
+
+namespace ne
+{
+namespace message_overlay
+{
+static std::uint32_t create (const std::string& title, const std::string& text, const std::vector<std::string>& button_texts)
+{
+  return static_cast<std::uint32_t>(vr::VROverlay()->ShowMessageOverlay(
+    text .c_str(), 
+    title.c_str(), 
+    button_texts.size() > 0 ? button_texts[0].c_str() : nullptr,
+    button_texts.size() > 1 ? button_texts[1].c_str() : nullptr,
+    button_texts.size() > 2 ? button_texts[2].c_str() : nullptr,
+    button_texts.size() > 3 ? button_texts[3].c_str() : nullptr));
+}
+static void          destroy()
+{
+  vr::VROverlay()->CloseMessageOverlay();
+}
+}
+}
+
+#endif
