96c7637d362bb3df12dc9dffc016321dcf60da2a
demiralp <demiralp@vr.rwth-aachen.de>
2018-07-09 11:41:39 +0200

    Updated SDL version to 2.0.8.

--- conanfile.py
+++ conanfile.py
@@ -4,13 +4,13 @@ import os
 
 class DiConan(ConanFile):
     name        = "di"
-    version     = "1.2.3"                     
+    version     = "1.3.0"                     
     description = "Conan package for di."           
     url         = "https://github.com/acdemiralp/di"
     license     = "MIT"                         
     settings    = "arch", "build_type", "compiler", "os"
     requires    = (("openvr/1.0.14@acdemiralp/makina"),
-                   ("sdl2/2.0.7@bincrafters/stable"))
+                   ("sdl2/2.0.8@bincrafters/stable"))
 
     def source(self):
         zip_name = "%s.zip" % self.version
